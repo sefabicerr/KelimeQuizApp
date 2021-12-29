4c53251f4814d61cd77d4f7b329a71b29fc15889
Muhammed Sefa Bicer <sefaabicerr@gmail.com>
2021-12-29 09:45:11 +0300

    Initial Commit

---
+++ KelimeQuiz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KelimeQuiz
+//
+//  Created by Muhammed Sefa Biçer on 29.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
